8afeca963e619df63610d4c935e9f3bd2c9e4acd
Pau Fracés <pfraces@gmail.com>
2016-02-14 19:10:51 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+ngl.log
+=======
+
+Log component
+
+Install
+-------
+
+    bower install ngl.log
+
+API
+---
+
+### `nglLog`
+
+Send a message to be logged
+
+```js
+nglLog(msg);
+```
+
+### `<ngl-log>`
+
+Show the messages sent with `nglLog` service
+
+```html
+<div ngl-log></div>
+```
---
+++ src/log.js
@@ -0,0 +1,38 @@
+angular.module('ngl.log', [
+  'ngl.log.templates'
+])
+
+.factory('nglLogCache', function () {
+  'use strict';
+
+  return [];
+})
+
+.factory('nglLog', function ($injector) {
+  'use strict';
+
+  var nglLogCache = $injector.get('nglLogCache');
+
+  var log = function (msg) {
+    if (typeof msg !== 'string') { msg = angular.toJson(msg, 2); }
+    nglLogCache.push(msg);
+  };
+
+  return log;
+})
+
+.directive('nglLog', function ($injector) {
+  'use strict';
+
+  var nglLogCache = $injector.get('nglLogCache');
+
+  var controller = function ($scope) {
+    $scope.log = nglLogCache;
+  };
+
+  return {
+    scope: true,
+    controller: controller,
+    templateUrl: 'ngl.log/log.html'
+  };
+});
